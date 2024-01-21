ab0c03d1e7fa03eeb835613d16efc7b82fc4d5f0
raaghassi <raaghassi@gmail.com>
2024-01-20 19:22:35 -0600

    Removed override, added numcpu and memory to variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,3 +33,15 @@ variable "domain" {
     type = string
     default = "aghassi.home"
 }
+
+variable "numcpu" {
+    description = "Number of CPUs (overrides template)"
+    type = string
+    default = "4"
+}
+
+variable "memory" {
+    description = "RAM size in megabytes (overrides template)"
+    type = string
+    default = "4096"
+}
